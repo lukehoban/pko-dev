5899df91bffae6cf9db0c957f2bac98e9ee8c5f2
Luke Hoban <lukehoban@gmail.com>
2022-10-18 21:58:46 -0700

    Update index.ts

--- basic/index.ts
+++ basic/index.ts
@@ -1,11 +1,14 @@
 import * as k8s from "@pulumi/kubernetes";
 import * as kx from "@pulumi/kubernetesx";
+import * as pulumi from "@pulumi/pulumi";
 
 const appLabels = { app: "nginx" };
+const config = new pulumi.Config();
+const replicas = config.getNumber("replicas") || 2;
 const deployment = new k8s.apps.v1.Deployment("nginx", {
     spec: {
         selector: { matchLabels: appLabels },
-        replicas: 2,
+        replicas: replicas,
         template: {
             metadata: { labels: appLabels },
             spec: { containers: [{ name: "nginx", image: "nginx" }] }
